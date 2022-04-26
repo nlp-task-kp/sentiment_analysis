e0a02eede931d9c2b491f9a37ca3b07a7bb0c3dd
Katarzyna Pekala <git@pekala.net>
2022-04-26 19:53:30 +0200

    added data_import

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
+import warnings
+import pandas as pd
+import numpy as np
+import toml
+
+
+def main():
+    data_import("data/sentences_with_sentiment.xlsx") #TODO: move to config
+    return None
+
+def data_import(input_path):
+    """
+    importing dataset with EPARs
+    :param input_path: path to dataset
+    """
+
+    print(f'Importing dataset from: {input_path}')
+    imported_sentences = pd.read_excel(input_path)
+    print(f'Imported dataset dimensions: {imported_sentences.shape}')
+
+    # couple of simple sanity checks
+    if imported_sentences.empty:
+        raise ValueError('Imported data frame is empty')
+
+    if any(imported_sentences[['Positive', 'Neutral', 'Negative']].sum(1) != 1):
+        warnings.warn("every sentence should have only one label.", UserWarning) #TODO: this should be handled
+
+    return imported_sentences
+
 if __name__ == '__main__':
-    print("data preprocessing")
+    main()
